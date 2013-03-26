21d78118849d18c2dbab3b23f77fa4bde26b5f69
Joseph Warner <hardolaf@hardolaf.com>
2013-03-26 19:16:37 -0400

    [Robot] Calibrate Encoders written

--- Proteus/robotdebug.cpp
+++ Proteus/robotdebug.cpp
@@ -1,4 +1,6 @@
 #include "robotdebug.h"
+#include "robotnormal.h"
+#include <FEHLCD.h>
 
 RobotDebug::RobotDebug()
 {
@@ -10,14 +12,48 @@ RobotDebug::RobotDebug(bool calibrate)
     RobotNormal::setup(calibrate);
 }
 
-void calibrateEncoders() {
+void RobotDebug::calibrateEncoders() {
+    LCD.Clear( FEHLCD::Black );
+    LCD.SetFontColor( FEHLCD::White );
+
+    int leftMin = 65535, rightMin = 65535;
+    int leftMax = 0, rightMax = 0;
+
+    while (!buttons->MiddlePressed()) {
+        if (leftMin > leftEncoder->Value()) {
+            leftMin = leftEncoder->Value();
+        }
+        if (leftMax < leftEncoder->Value()) {
+            leftMax = leftEncoder->Value();
+        }
+        if (rightMin > rightEncoder->Value()) {
+            rightMin = rightEncoder->Value();
+        }
+        if (rightMax > rightEncoder->Value()) {
+            rightMax = rightEncoder->Value();
+        }
+
+        // Print
+        LCD.Clear( FEHLCD::Black );
+        LCD.WriteLine("Encoder Calibration");
+        LCD.WriteLine("  Press middle button to stop.");
+        LCD.WriteLine("Right:");
+        LCD.Write("Min: ");
+        LCD.Write(rightMin);
+        LCD.Write("   Max: ");
+        LCD.WriteLine(rightMax);
+        LCD.WriteLine("Left:");
+        LCD.Write("Min: ");
+        LCD.Write(leftMin);
+        LCD.Write("   Max: ");
+        LCD.WriteLine(leftMax);
+    }
+}
+
+void RobotDebug::calibrateOptosensors() {
 
 }
 
-void calibrateOptosensors() {
-
-}
-
-void calibrateLightSensor() {
+void RobotDebug::calibrateLightSensor() {
 
 }
