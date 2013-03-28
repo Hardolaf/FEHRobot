1b8870e10b637190f896db20e0c07cfbd25f5aec
Joseph Warner <hardolaf@hardolaf.com>
2013-03-28 13:20:04 -0400

    [Robot] Formatting

--- Proteus/robot.h
+++ Proteus/robot.h
@@ -7,9 +7,12 @@ public:
     virtual void movementStraight(int, float) = 0;
     virtual void movementLeft(int) = 0;
     virtual void movementRight(int) = 0;
+
     static const float ENCODER_COUNTS_PER_INCH = 3.316344860705540019742322139111;
     static const float ENCODER_COUNTS_PER_DEGREE_TURN = 0.23;
+
     static const int MOVEMENT_MOTOR_TURN_SPEED = 80;
+
     static const float LIGHT_SENSOR_MAX_LIGHT_VALUE = 1.200;
 
     static const float OPTOSENSOR_LEFT_LOW_THRESHOLD = 2.000;
